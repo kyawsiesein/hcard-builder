dc6fb9bbf033360035cb14d0e768ccd6781e7810
Kyaw Siesein <kyawsiesein@kyaws-mbp.lan>
2019-05-01 23:39:08 +1000

    format the code

--- src/App.js
+++ src/App.js
@@ -5,48 +5,52 @@ import Preview from "./components/Preview";
 import "./styles/app.scss";
 
 const App = () => {
-  const [details, setDetails] = useState({
-    givenName: "",
-    surname: "",
-    email: "",
-    phone: "",
-    houseName: "",
-    street: "",
-    postcode: "",
-    suburb: "",
-    state: "",
-    country: "",
-    imagePath: ""
-  });
+    const [details, setDetails] = useState({
+        givenName: "",
+        surname: "",
+        email: "",
+        phone: "",
+        houseName: "",
+        street: "",
+        postcode: "",
+        suburb: "",
+        state: "",
+        country: "",
+        imagePath: ""
+    });
 
-  const onChange = event => {
-    const { name, value } = event.target;
-    setDetails({ ...details, [name]: value });
-  };
-
-  const onUploadFile = event => {
-    const file = event.target.files[0];
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = () => {
-      const path = fileReader.result;
-      setDetails({ ...details, imagePath: path });
+    const onChange = event => {
+        const { name, value } = event.target;
+        setDetails({ ...details, [name]: value });
     };
-  };
 
-  return (
-    <Container fluid>
-      <Row>
-        <Col xs={12} md={6} className="hcard-form">
-          <h1 className="hcard-header">hCard Builder</h1>
-          <Form details={details} onChange={onChange} onUploadFile={onUploadFile} />
-        </Col>
-        <Col xs={12} md={6} className="hcard-preview">
-          <Preview details={details} />
-        </Col>
-      </Row>
-    </Container>
-  );
+    const onUploadFile = event => {
+        const file = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            const path = fileReader.result;
+            setDetails({ ...details, imagePath: path });
+        };
+    };
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col xs={12} md={6} className="hcard-form">
+                    <h1 className="hcard-header">hCard Builder</h1>
+                    <Form
+                        details={details}
+                        onChange={onChange}
+                        onUploadFile={onUploadFile}
+                    />
+                </Col>
+                <Col xs={12} md={6} className="hcard-preview">
+                    <Preview details={details} />
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default App;
